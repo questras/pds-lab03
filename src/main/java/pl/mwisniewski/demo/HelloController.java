653a9f7abba0ba3ea9bd4d4d9ac255d8bccde711
Michal Wisniewski <michu123qwe@gmail.com>
2022-04-21 17:02:26 +0200

    Change message.

--- src/main/java/pl/mwisniewski/demo/HelloController.java
+++ src/main/java/pl/mwisniewski/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Greetings from spring boot!";
+        return "Hello, world!";
     }
 }
--- src/test/java/pl/mwisniewski/demo/HelloControllerTest.java
+++ src/test/java/pl/mwisniewski/demo/HelloControllerTest.java
@@ -23,6 +23,6 @@ public class HelloControllerTest {
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Greetings from spring boot!")));
+                .andExpect(content().string(equalTo("Hello, world!")));
     }
 }
